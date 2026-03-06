7b1258fa00174d8f003c0d0a24ab58b25d14f1ef
Jorge Sanchez <tu-email@ejemplo.com>
2026-03-06 16:32:53 -0500

    add actionsv35vuln

--- helpers/unsafe.js
+++ helpers/unsafe.js
@@ -1,10 +1,29 @@
-const http = require('http');
+import { Router } from "express";
+import expressJwt from 'express-jwt'
 
-function handleRequest(req, res) {
-  const userInput = req.url.split('?q=')[1];
-  // Unsafe: code injection via eval
-  const result = eval(userInput);
-  res.end(String(result));
+// F034: Predictable Math.random used for security
+export const denyAll = () => expressJwt({ secret: '' + Math.random() })
+
+function unsafe_route(req, res) {
+  let key = Math.random().toString();
+  res.cookie("rememberKey", key);
+  res.json({ ok: true });
 }
 
-http.createServer(handleRequest).listen(3001);
+// F143: eval with untrusted input
+function insec_use_of_eval(req) {
+    let input = req.query.input;
+    try {
+        eval('alert("Your query string was ' + unescape(document.location.search) + '");');
+        eval(req.query.input)
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+// F021: XSS - reflected
+function handleSearch(req, res) {
+    const query = req.query.q;
+    res.send('<h1>Results for: ' + query + '</h1>');
+}
